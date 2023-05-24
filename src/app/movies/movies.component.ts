950dcfeac1727f87372fa20c5b704a968c7e0f88
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-05-24 00:21:20 -0700

    DIRECTORS AND MOVIES GUI'S COMPLETED

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Movie } from '../interfaces/movie';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent {
+  movies: Movie[] = [];
+  movie: Movie = {
+    Director: 0,
+    Duration: '',
+    Gender: 'terror',
+    Name: '',
+    Release_Year: new Date(),
+  };
+  constructor() {}
 
+  saveMovie() {}
+  updateMovie() {}
+  deleteMovie() {}
+  getMovie() {}
 }
